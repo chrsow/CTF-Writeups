4c54c07e05a9d4d58422d625c96f3080731c32af
chrsow <liang_wasin@hotmail.co.th>
2018-03-24 22:56:29 +0700

    Add xor.py, angstorm18

---
+++ 2018-angstromctf/crypto/xor.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+import codecs
+# def or()
+c = codecs.decode('fbf9eefce1f2f5eaffc5e3f5efc5efe9fffec5fbc5e9f9e8f3eaeee7','hex')
+k = chr(c[0]^ord('a'))
+flag = ''
+for i in c:
+    flag += chr(i^ord(k))
+print(flag)
